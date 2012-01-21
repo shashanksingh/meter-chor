6c9e8d3f153af599e174ec936db9f1ed49786102
shashank singh <shashank.personal@gmail.com>
2012-01-22 05:09:26 +0530

    server is now in server.js and index.js is our int main()

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 var http = require("http");
 
-http.createServer(function(request, response) {
-    response.writeHead(200, {"Content-Type" : "text/plain"});
-    response.write("Hello World");
-    response.end();
-}).listen(8000);
+function start() {
+    http.createServer(function(request, response) {
+        response.writeHead(200, {"Content-Type" : "text/plain"});
+        response.write("Hello World");
+        response.end();
+    }).listen(8000);
+
+console.log("Server has started.");
+}
+
+exports.start = start;
 
