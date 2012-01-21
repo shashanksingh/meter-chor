1a4da8dc3bce0d290bc6445639bb4af278f082a8
shashank singh <shashank.personal@gmail.com>
2012-01-22 05:29:54 +0530

    we now have request handlers ouptutting to console.log

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var http = require("http");
+var url = require("url");
 
 function start() {
     http.createServer(function(request, response) {
+        var pathname = url.parse(request.url).pathname
+        console.log("New Request for ", pathname);
         response.writeHead(200, {"Content-Type" : "text/plain"});
         response.write("Hello World");
         response.end();
